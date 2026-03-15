7ac3ea375f957f3e9babb1996bde8db5233aea31
s3mpr3no1 <crino.nicholas@gmail.com>
2026-03-14 20:47:24 -1000

    initial refactor for version 2

---
+++ src_v2/notes.md
@@ -0,0 +1,5 @@
+Data schema: 
+
+Card
+- Front: string
+- Back: string
---
+++ src_v2/script.js
@@ -0,0 +1,9 @@
+
+// export type SuperMemoItem = {
+//     interval: Number;
+//     repetition: Number;
+//     efactor: Number;
+// };
+
+// export type SuperMemoGrade = 0 | 1 | 2 | 3 | 4 | 5;
+
